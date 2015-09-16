56d5ddca8548fdb3d3bae097c3598124ba1640e9
Matt Mo <matthew.l.mo@gmail.com>
2015-09-15 18:40:57 -0700

    Set up primary structure for navigation and table view

---
+++ Rotten Tomatoes/Rotten Tomatoes/MovieTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MovieTableViewCell.swift
+//  Rotten Tomatoes
+//
+//  Created by admin on 9/15/15.
+//  Copyright © 2015 mattmo. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+}
+
+
---
+++ Rotten Tomatoes/Rotten Tomatoes/MovieTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Rotten Tomatoes
+//
+//  Created by admin on 9/14/15.
+//  Copyright © 2015 mattmo. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.registerClass(MovieTableViewCell.self, forCellReuseIdentifier: "cell");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath);
+        cell.textLabel?.text = "blah \(indexPath.row)";
+        return cell;
+    }
+}
+
